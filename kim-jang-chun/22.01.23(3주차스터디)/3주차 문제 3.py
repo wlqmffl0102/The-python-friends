b01a018bac25c4790974845f741225a8050a59e8
o3693k <86004523+o3693k@users.noreply.github.com>
2022-01-28 13:25:02 +0900

    4주차 업로드

---
+++ kim-jang-chun/22.01.23(3주차스터디)/3주차 문제 3.py
@@ -0,0 +1,15 @@
+a = "20"
+b = "21"
+c = "22"
+d = "23"
+e = "24"
+f = "25"
+g = "26"
+h = "27"
+i = "28"
+j = "29"
+
+int(a[0])*int(a[1])+int(b[0])*int(b[1])+int(c[0])*int(c[1])+int(d[0])*int(d[1])+int(e[0])*int(e[1])+int(f[0])*int(f[1])+int(g[0])*int(g[1])+int(h[0])*int(h[1])+int(i[0])*int(i[1])+int(j[0])*int(j[1])
+
+print(a+b+c+d+e+f+g+h+i+j)
+
