a79064acb5529a46184a33611e33de89fa4c4ef8
HongJuWon <96655483+HongJuWon@users.noreply.github.com>
2022-01-29 16:13:10 +0900

    0129_test

---
+++ Hong-Ju-Won/0129_test/0129_03.py
@@ -0,0 +1,16 @@
+# 피보나치 수열
+# 1 1 2 3 5 8 13 21 34 55 출력
+
+n = 1
+n1 = 1
+n2 = n+n1
+n3 = n1+n2
+n4 = n2+n3
+n5 = n3+n4
+n6 = n4+n5
+n7 = n5+n6
+n8 = n6+n7
+n9 = n7+n8
+
+
+print(int(n) + " " + int(n1) + " " + int(n2) + " " + int(n3) + " " + int(n4) + " " + int(n5) + " " + int(n6) + " " + int(n7) + " " + int(n8) + " " + int(n9))
