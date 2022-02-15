214bea9806835c1fc83abb9f0167e03c03dcbe12
HongJuWon <96655483+HongJuWon@users.noreply.github.com>
2022-02-15 23:06:35 +0900

    (220226)for문 예제

---
+++ Hong-Ju-Won/0226_star.py/star_for1.py
@@ -0,0 +1,13 @@
+# *** 2줄씩 출력
+
+for b in range(2):
+    for a in range(3):
+        print("*", end="")
+    print(" ")
+
+print("-"*50)
+
+for i in range(1,6):
+    for j in range(i):
+        print("*", end="")
+    print(" ")
---
+++ Hong-Ju-Won/0226_star.py/star_for2.py
@@ -0,0 +1,11 @@
+for i in range(1,6):
+    for j in range((6-i),0,-1):
+        print("*", end="")
+    print(" ")
+
+print("-"*50)
+
+# for b in range(2):
+#     for a in range(3):
+#         print("*", end="")
+#     print(" ")
