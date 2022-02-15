d5f688ece9e8e853d5647042c6861e34afb32886
HongJuWon <96655483+HongJuWon@users.noreply.github.com>
2022-02-15 23:05:59 +0900

    (220206)while 예제

---
+++ Hong-Ju-Won/0206_test/while1.py
@@ -0,0 +1,7 @@
+while True:
+    print("Hello")
+
+# while 무한 루프 예시
+# ctrl+C 하면 빠져 나옴
+# True/False 첫 글자 대문자
+
---
+++ Hong-Ju-Won/0206_test/while2.py
@@ -0,0 +1,9 @@
+a = True
+n = 0
+while a:
+    n=n+1
+    print("Hello")
+    if n>5:
+        a=False
+# if 조건 주면 무한 루프 안에서 멈춤
+
